5cee18c02c342318dbedf522391a94940d092f82
Chris Guidry <chris@theguidrys.us>
2014-09-28 20:52:55 -0400

    A little more licensing stuff.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name='kademlia_aio',
     version='0.1',
     description='The Kademlia distributed hash table, implemented for Python 3.4 asyncio.',
+    url='https://github.com/chrisguidry/kademlia_aio',
+    license='MIT',
     author='Chris Guidry',
     author_email='chris@theguidrys.us',
     packages=['kademlia_aio'],
